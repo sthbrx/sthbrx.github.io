27d03e27ddc8ed9ce1522f784f2ace3feb5e332a
Callum Scarvell <callum.s@live.com.au>
2016-02-17 11:07:13 +1100

    initial commit, pelican

---
+++ pelicanconf.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'OzLabs'
+SITENAME = u'Store Half Byte-Reverse Indexed'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Australia/Canberra'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 2
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
